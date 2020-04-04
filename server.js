ba8af75b902a6a603e2b908a797b226ad6e2d557
Bosco Pan <boscpan@gmail.com>
2020-04-04 09:18:16 -0700

    update documentation in each endpoint

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// Find events with the given filters on location, title,
+// organizerName, types, startDate, endDate
 app.post('/api/events', (req, res) => {
   console.log(req.body);
   res.send(
@@ -40,25 +41,39 @@ app.post('/api/events', (req, res) => {
   )
 })
 
+// Create an event given its organizerID, title, location,
+// types (as an array of type), startDate, endDate, description
 app.post('/api/event', (req, res) => {
   console.log(req.body);
   res.send();
 })
 
+// Delete the event with given eventID
 app.delete('/api/event/:eventID', (req, res) => {
   console.log(req.params.eventID);
   res.send();
 })
 
+// User with userID joins event with eventID. Create such
+// entry in Participate table
 app.put('/api/event/:eventID/user/:userID', (req, res) => {
   console.log(req.params.eventID);
   console.log(req.params.userID);
   res.send();
 })
 
+// Create a user with the given firstName, lastName, dateOfBirth,
+// gender, cardNumber, expiryDate, holderName and CVC
 app.post('/api/user', (req, res) => {
   console.log(req.body);
   res.send({ userID: 0 });
+})
+
+// Send status code 200 if a user with the given userID exists,
+// 404 otherwise.
+app.get('/api/user/:userID', (req, res) => {
+  console.log(req.params.userID);
+  res.send();
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
